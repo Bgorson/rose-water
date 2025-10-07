e1380642ff5f601c9dba9998bf1e72aa58d28f6e
Brandon Gorson <bgorson32@gmail.com>
2025-10-06 20:05:16 -0500

    update location

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -55,20 +55,20 @@ const Locations = () => {
   return (
     <Container>
       <MainContainer>
-        <h2>Bucktown Office</h2>
+        <h2>Logan Square/Bucktown Office</h2>
         <HorizontalContainer>
           <OfficeImage src={BucktownOfficeImage} alt="Bucktown Office" />
           <OfficeImage src={BucktownOfficeImage2} alt="Bucktown Office 2" />
         </HorizontalContainer>
         <iframe
-          title="Bucktown Office"
-          width={"100%"}
+          title="Logan Square/Bucktown Office"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.4651538106277!2d-87.6873967!3d41.9258551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd29006a7e555%3A0x90007284c5a1efaf!2s2433%20N%20Western%20Ave%2C%20Chicago%2C%20IL%2060647!5e0!3m2!1sen!2sus!4v1759798940909!5m2!1sen!2sus"
+          width="600"
           height="450"
-          frameBorder="0"
           style={{ border: 0 }}
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.6600263928267!2d-87.67622272341389!3d41.921666662684466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2ec1036710f%3A0xc976ad1d18ea89b2!2s1820%20W%20Webster%20Ave%20Suite%20400%2C%20Chicago%2C%20IL%2060614!5e0!3m2!1sen!2sus!4v1708462400902!5m2!1sen!2sus"
-          allowFullScreen
-        ></iframe>
+          allowfullscreen
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
       </MainContainer>
       <MainContainer>
         <h2>Skokie Office</h2>
